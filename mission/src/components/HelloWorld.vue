5a04898ddafa9e637a25a8b3a7855e9dede76644
minsun <meeeeensun@gmail.com>
2022-01-08 19:40:03 +0900

    alert warning 해결 / 컴포넌트화

--- mission/src/components/HelloWorld.vue
+++ mission/src/components/HelloWorld.vue
@@ -1,47 +1,20 @@
 <template>
   <div class="hello">
     <h1>{{ msg }}</h1>
-    <div>
-      <input v-model="input" />
-      <button @click="onSubmit" class="button-style">제출</button>
-      <button @click="openAlert" class="button-style">알림</button>
-    </div>
-    <div class="input-style">
-      <span>{{ input }}</span>
-    </div>
+    <StringItem />
   </div>
 </template>
 
 <script>
-import { reactive, toRefs } from 'vue';
+import StringItem from '@/components/StringItem.vue';
 
 export default {
   name: 'HelloWorld',
   props: {
     msg: String,
   },
-  setup() {
-    const state = reactive({
-      input: '',
-      count: 0,
-    });
-
-    const methods = reactive({
-      onSubmit: () => {
-        const first = state.input.substring(0, 1);
-        const rest = state.input.substring(1, state.input.length);
-        state.input = rest + first;
-      },
-      openAlert: () => {
-        state.count += 1;
-        alert(`${state.input} ${state.count}`);
-      },
-    });
-
-    return {
-      ...toRefs(state),
-      ...toRefs(methods),
-    };
+  components: {
+    StringItem,
   },
 };
 </script>
@@ -61,14 +34,5 @@ li {
 }
 a {
   color: #42b983;
-}
-
-.button-style {
-  margin-left: 5px;
-}
-
-.input-style {
-  margin-top: 15px;
-  font-size: 16px;
 }
 </style>
---
+++ mission/src/components/StringItem.vue
@@ -0,0 +1,80 @@
+<template>
+  <div>
+    <div>
+      <input v-model="rotateString" />
+      <button @click="rotate" class="button-style">제출</button>
+      <input type="button" @click="openAlert" value="알림" class="button-style" />
+    </div>
+    <div class="input-string-style">
+      <span data-test="rotate-string">{{ rotateString }}</span>
+    </div>
+    <dialog id="alert-dialog">
+      <div>{{ rotateString }} {{ count }}</div>
+      <button @click="closeAlert" class="close-button">확인</button>
+    </dialog>
+  </div>
+</template>
+
+<script>
+import {
+  reactive,
+  toRefs,
+} from 'vue';
+
+export default {
+  name: 'StringItem',
+  setup() {
+    const state = reactive({
+      rotateString: '',
+      count: 0,
+      open: false,
+    });
+
+    const methods = reactive({
+      rotate: () => {
+        const first = state.rotateString.substring(0, 1);
+        const rest = state.rotateString.substring(1);
+        state.rotateString = rest + first;
+      },
+      openAlert: () => {
+        state.count += 1;
+        state.open = true;
+        const alertDialog = document.getElementById('alert-dialog');
+        alertDialog.showModal();
+      },
+      closeAlert: () => {
+        const alertDialog = document.getElementById('alert-dialog');
+        alertDialog.close();
+      },
+    });
+
+    return {
+      ...toRefs(state),
+      ...toRefs(methods),
+    };
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.button-style {
+  margin-left: 5px;
+}
+
+.input-string-style {
+  margin-top: 15px;
+  font-size: 16px;
+}
+
+#alert-dialog {
+  width: 400px;
+  height: 45px;
+  border: 1px solid black;
+}
+
+.close-button {
+  position: absolute;
+  right: 35px;
+}
+</style>
