50f17d53bc25f21a74f9795e70e365b61045c50d
minsun <meeeeensun@gmail.com>
2022-01-14 02:55:54 +0900

    resolve conflicts

--- mission/src/App.vue
+++ mission/src/App.vue
@@ -1,15 +1,5 @@
 <template>
-<<<<<<< HEAD
-  <div>
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
     <router-view />
-  </div>
-=======
-  <router-view />
->>>>>>> 1377ebc8fc47eb8797d0845d3bfb3f68eb37c18b
 </template>
 
 <style>
